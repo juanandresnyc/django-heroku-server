841fc053812e692cfc07dcf574f80c198c13b06e
juan andres andrango <andrango.juanandres@gmail.com>
2019-02-11 00:28:27 -0500

    Set postgresql as db

--- project/project/settings.py
+++ project/project/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'project'),
+        'USER': os.environ.get('DB_USER', 'staging_user'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'password'),
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
 }
 
 
